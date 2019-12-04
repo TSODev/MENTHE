0b7721658f41b615a29f16309a2e8d4a549ae601
Thierry Soulie <thierry@tsodev.fr>
2019-12-04 19:10:41 +0100

    incoming variables mapping

--- MENTHE_CLIENT/src/app/workflow/analysis/incoming/incoming.component.ts
+++ MENTHE_CLIENT/src/app/workflow/analysis/incoming/incoming.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { AnalysisService } from 'src/app/_services/analysis.service';
+import { PublishingService } from 'src/app/_services/publishing.service'
 import { SequenceFlow } from 'src/app/_models/bpmn';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { SubSink } from 'subsink';
+import { Variable } from 'src/app/_interfaces/publish.interface';
 
 @Component({
   selector: 'app-incoming',
   templateUrl: './incoming.component.html',
   styleUrls: ['./incoming.component.css']
 })
-export class IncomingComponent implements OnInit {
+export class IncomingComponent implements OnInit, OnDestroy {
 
   @Input()
     incoming: string;
@@ -18,17 +23,46 @@ export class IncomingComponent implements OnInit {
     haslink = false;
     links: SequenceFlow[];
 
-  constructor(
-    private analysisService: AnalysisService,
-  ) { }
+    variableForm: FormGroup;
+    variables: Variable[] = [];
+    variables$: Observable<Variable>;
 
-  ngOnInit() {
-    this.linked = this.analysisService.getLinkedFlowFromElementList(this.incoming);
-//    console.log(this.incoming , 'Linked To: ', this.linked);
-    if (typeof this.linked !== 'undefined') {
-      this.haslink = true;
-      this.links = this.linked;
+    subs = new SubSink();
+
+
+    constructor(
+      private formBuilder: FormBuilder,
+      private analysisService: AnalysisService,
+      private publishingService: PublishingService,
+    ) {
+      this.variableForm = this.formBuilder.group({
+        variableType: ['', Validators.required],
+        name: ['', Validators.required],
+        defaultValue: [''],
+      });
+
+      this.variables$ = this.publishingService.variables$;
+
+      this.subs.add(this.publishingService.variables$.subscribe(
+        data => {
+          this.variables.push(data);
+        }
+      ));
+     }
+
+    ngOnInit() {
+      this.linked = this.analysisService.getLinkedFlowFromElementList(this.incoming);
+      if (typeof this.linked !== 'undefined') {
+        this.haslink = true;
+        this.links = this.linked;
+      }
+      const workflowId = this.analysisService.getElementList().wf.workflow_id;
+      const processes = this.analysisService.getElementList().proc;
+      this.variables = this.variables.filter(w => w.workflowId === workflowId);
     }
-  }
 
-}
+    ngOnDestroy() {
+      this.subs.unsubscribe();
+    }
+
+  }
