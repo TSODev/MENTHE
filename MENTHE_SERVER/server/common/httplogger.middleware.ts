2a8ec882b1a8a09c62904a83f4f5f06c1eeed574
Thierry Soulie <thierry@tsodev.fr>
2019-12-07 22:53:47 +0100

    remove logger for OPTIONS request

--- MENTHE_SERVER/server/common/httplogger.middleware.ts
+++ MENTHE_SERVER/server/common/httplogger.middleware.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import l from './logger';
 
 export function httpLogger(req:Request, res:Response, next: NextFunction) {
-    l.debug('HTTP Logger Request : ', req.method + " - " + req.url + ' by ' + JSON.stringify(req['userId']));
+    if (req.method !=='OPTIONS') {
+    l.debug('HTTP Logger: ', req.method + " - " + req.url + ' by ' + JSON.stringify(req['userId']));
+    }
     next();
 }
